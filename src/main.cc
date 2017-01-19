318dfd002a8a67bfa6ccce0c788ffc5d9b9aa5a9
git-rep <git-rep@openmailbox.org>
2017-01-19 15:58:37 -0300

    Add message.

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,17 @@
 #include "nss.h"
 
+#include <iostream>
+
 int main (int argc, char *argv[])
 {	
+    if (argc > 1) {
+        std::cout << std::endl;
+        std::cout << "Exploit-DB's (Offensive Security) Ncurses search tool (https://github.com/git-rep/nss)" << std::endl;
+        std::cout << "Version 0.1" << std::endl;
+        std::cout << std::endl;
+        return 0;
+    }
+    
     Nss nss;
     
     return 0;
