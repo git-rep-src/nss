931b0120b14014cb393d94adbac3476e9b26805e
git-rep-src <git-rep@protonmail.com>
2021-04-09 15:16:53 -0300

    Update URL.

--- src/main.cc
+++ src/main.cc
@@ -6,8 +6,7 @@ int main (int argc, char *argv[])
 {	
     if (argc > 1) {
         std::cout << std::endl;
-        std::cout << "Exploit-DB's Offensive Security Ncurses search tool " <<
-                     "(https://github.com/git-rep/nss)" << std::endl;
+        std::cout << "Exploit-DB's Offensive Security Ncurses search tool (https://github.com/git-rep-src/nss)" << std::endl;
         std::cout << "Version 0.1" << std::endl;
         std::cout << std::endl;
         return 0;
